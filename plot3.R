c4addfc4914998d5a38566370b60481bf98a594f
gfab <fdek2@verizon.net>
2014-06-08 12:53:38 -0400

    update

--- plot3.R
+++ plot3.R
@@ -2,7 +2,6 @@ p<-read.csv("household_power_consumption.txt",
   header=TRUE, sep=";", na.strings=c("?"))
 pp<-p[p$Date=="1/2/2007" | p$Date=="2/2/2007",]
 pp$Date=as.Date(pp$Date, format="%d/%m/%Y")
-pp$DoW=format(pp$Date, format="%a")
 pp$datetime<-strptime(paste(pp$Date, pp$Time),
   "%Y-%m-%d %H:%M:%S")
 png(filename="plot3.png")
